ceb593d8110b6798071b534b43f8b801501c8274
Zheng <1208444648@qq.com>
2017-06-10 01:04:15 +0800

    17.6.9 添加热门模板接口

--- app/src/main/java/com/wq/template/AddTemplateActivity.java
+++ app/src/main/java/com/wq/template/AddTemplateActivity.java
@@ -15,13 +15,24 @@ import com.bilibili.boxing.Boxing;
 import com.bilibili.boxing.model.config.BoxingConfig;
 import com.bilibili.boxing.model.entity.BaseMedia;
 import com.bilibili.boxing_impl.ui.BoxingActivity;
+import com.google.gson.reflect.TypeToken;
+import com.sunrun.sunrunframwork.bean.BaseBean;
 import com.sunrun.sunrunframwork.uiutils.PictureShow;
+import com.sunrun.sunrunframwork.uiutils.ToastUtils;
 import com.sunrun.sunrunframwork.uiutils.UIUtils;
+import com.sunrun.sunrunframwork.utils.formVerify.FormatException;
+import com.sunrun.sunrunframwork.utils.formVerify.VerifyUtil;
+import com.sunrun.sunrunframwork.utils.formVerify.VerifyerSet;
 import com.sunrun.sunrunframwork.view.ItemView;
 import com.sunrun.sunrunframwork.view.title.BaseTitleLayoutView;
 import com.sunrun.sunrunframwork.weight.MultiImageUploadView;
 import com.sunrun.sunrunframwork.weight.switchbtn.SlideSwitch;
 import com.wq.base.LBaseActivity;
+import com.wq.common.model.GroupListObj;
+import com.wq.common.model.TeamTemplateListObj;
+import com.wq.common.quest.BaseQuestConfig;
+import com.wq.common.quest.BaseQuestStart;
+import com.wq.common.util.IntentUtil;
 import com.wq.common.widget.TitleBar;
 import com.wq.project01.R;
 
@@ -29,10 +40,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static com.sunrun.sunrunframwork.utils.formVerify.VerifyUtil.verify;
 
 /**
  * Created by weiquan on 2017/6/2.
@@ -56,6 +70,11 @@ public class AddTemplateActivity extends LBaseActivity {
     @BindView(R.id.activity_main)
     LinearLayout activityMain;
     int IMG_REQUEST_CODE = 0x010;
+
+
+    private List<String> list;
+    private String name;
+    private String is_open = "-1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,13 +105,75 @@ public class AddTemplateActivity extends LBaseActivity {
                         start(that, IMG_REQUEST_CODE);
             }
         });
+        titleBar.setRightAction(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                String title = tvTitle.getText().toString().trim();
+//                String content = editContent.getText().toString().trim();
+               
+
+                if (list ==null && name == null){
+                    list = new ArrayList<String>();
+                    name = " ";
+                }
+
+                is_open =  cbGongkai.isSelected() ?  "1": "-1";
+
+                    try {
+                        verify(tvTitle, new VerifyerSet.EmptyVerifyer("请输入模板名称"));
+                        verify(editContent,new VerifyerSet.EmptyVerifyer("请输入分享内容"));
+                    } catch (FormatException e) {
+                        e.printStackTrace();
+                        UIUtils.shortM(e.getLocalizedMessage());
+                    }
+                /**
+                 * 用户添加模板
+                 * API参数传入方式: POST
+                 * 传入JSON格式: {"template_name":"测试","template_content":"测试内容","template_cover_img":"base64code...","img_list":["base64code...","base64code..."],"remind_id_list":["uid1","uid2"],"is_open":"-1"}
+                 * 返回JSON格式: {"meta":{"code":200,"message":"success"}}
+                 *
+                 * @param template_name->九宫格模板名称标题
+                 * @param template_content->模板内容
+                 * @param template_cover_img->模板封面图片base64格式
+                 * @param img_list->图片list                   最少1张 最多9张 数据为base64格式
+                 * @param remind_id_list->提醒指定团队成员id         list
+                 * @param is_open->模板是否公开                    -1不公开 1公开 (可选传)
+                 * @return code 200->成功 3001->template_name参数为空 3002->template_content参数为空 3005->template_cover_img参数为空  3003->img_list图片至少1张最多9张 3004->img_list包含不合法文件 3006->template_cover_img包含不合法文件 3007->数据插入失败
+                 */
+               // BaseQuestStart.addTemplate(this,);
+
+            }
+        });
 
     }
 
+    @Override
+    public void nofityUpdate(int requestCode, BaseBean bean) {
+        switch (requestCode) {
+            case BaseQuestConfig.QUEST_GET_GROUP_LIST_CODE:
+                //log 设置 tag为NetServer 可以查看请求情况
+                if (bean.status == 200) {
+
+                } else {
+                    ToastUtils.shortToast("暂无团队信息");
+                }
+                break;
+        }
+
+        super.nofityUpdate(requestCode, bean);
+
+    }
 
     ArrayList<Bitmap> bitmaps = new ArrayList<Bitmap>();
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 123 && resultCode == RESULT_OK){
+
+            list = getSession().getBean("ids",new TypeToken<List<String>>(){});
+            name = getSession().getString("names");
+
+            itemRemind.setRightText(name);
+        }
         ArrayList<BaseMedia> images = Boxing.getResult(data);
         if (images != null) {
             for (BaseMedia image : images) {
@@ -124,11 +205,12 @@ public class AddTemplateActivity extends LBaseActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.item_gongkai:
+
                 break;
             case R.id.item_remind:
 
-//                Intent intent = new Intent(this,RemindActivity.class);
-//                startActivity();
+                IntentUtil.startSlectPeopleActivity(AddTemplateActivity.this , 123);
+
 
                 break;
             case R.id.titleBar:
